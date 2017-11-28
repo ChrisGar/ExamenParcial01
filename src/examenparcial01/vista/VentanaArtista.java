f37ac52fa473af2bddb6e912ee76633094556a0c
Eduardo <Eduardo@172.16.221.132>
2017-11-28 18:42:24 -0500

    se a creado: la clase EventoVentanaPrincipal y el EventoVentanaArtista

--- src/examenparcial01/vista/VentanaArtista.java
+++ src/examenparcial01/vista/VentanaArtista.java
@@ -5,7 +5,9 @@
  */
 package examenparcial01.vista;
 
+import examenparcial01.controlador.EventoVentanaArtista;
 import examenparcial01.controlador.GestionDato;
+import examenparcial01.modelo.Artista;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
@@ -45,26 +47,26 @@ public class VentanaArtista extends JInternalFrame {
     public void iniciacomponente(){
         this.etiquetaList=new ArrayList<JLabel>();
         this.etiquetaList.add(new JLabel("Codigo"));
-        this.etiquetaList.add(new JLabel("Nombre"));
-        this.etiquetaList.add(new JLabel("Apellidos"));
+        this.etiquetaList.add(new JLabel("Nombre y Apellido"));
+        this.etiquetaList.add(new JLabel("Fecha de Nacimiento"));
         this.etiquetaList.add(new JLabel("Genero de la musica"));
         this.txtList=new ArrayList<JTextField>();
-        this.txtList.add(new JTextField(8));
+        this.txtList.add(new JTextField(20));
         this.txtList.add(new JTextField(25));
-        this.txtList.add(new JTextField(10));
+        this.txtList.add(new JTextField(8));
         this.txtList.add(new JTextField(15));
         
         JPanel panel=new JPanel();
         
         this.botonList=new ArrayList<JButton>();
         this.botonList.add(new JButton("Guardar"));
-        
+        this.botonList.get(0).addActionListener(new EventoVentanaArtista(this));
         this.botonList.add(new JButton("Borrar"));
         
         this.encabezado=new Object[4];
         this.encabezado[0]="Cedula";
-        this.encabezado[1]="Nombre";
-        this.encabezado[2]="Apellidos";
+        this.encabezado[1]="Nombre Y Apellido";
+        this.encabezado[2]="Fecha de Nacimiento";
         this.encabezado[3]="Genero";
         
         this.modeloTabla=new DefaultTableModel(this.datos,this.encabezado);
@@ -84,6 +86,21 @@ public class VentanaArtista extends JInternalFrame {
         panel.add(this.scroll);  
         this.add(panel);
         
+    }
+    
+    public Object[][] cargardatos(int x,int y){
+
+        Object[][] retorno=new Object[x][y];
+        int i=0;
+        for (Artista a: this.gd.getArtistaList()){
+            retorno[i][0]=a.getCodigo();
+            retorno[i][1]=a.getNombreYApellido();
+            retorno[i][2]=a.getFechaNacimiento();
+            retorno[i][3]=a.getFechaNacimiento();
+            retorno[i][4]=a.getMusicagenero();
+            i++;
+        }
+    return retorno;
     }
 
     public List<JLabel> getEtiquetaList() {
