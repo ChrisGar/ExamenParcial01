036d0337b5b4c646ee5a16f0675252d8e4007c4e
Estudiante <Estudiante@lab0802>
2017-11-28 14:26:24 +0100

    se creo la clase Festival

--- src/examenparcial01/modelo/Boleto.java
+++ src/examenparcial01/modelo/Boleto.java
@@ -10,5 +10,49 @@ package examenparcial01.modelo;
  * @author Estudiante
  */
 public class Boleto {
+    private Asistente asistente;
+    private int numAsiento;
+    private char seccionAsiento;
+    private Festival festival; 
+
+    public Boleto(Asistente asistente, int numAsiento, char seccionAsiento, Festival festival) {
+        this.asistente = asistente;
+        this.numAsiento = numAsiento;
+        this.seccionAsiento = seccionAsiento;
+        this.festival = festival;
+    }
+
+    public Asistente getAsistente() {
+        return asistente;
+    }
+
+    public void setAsistente(Asistente asistente) {
+        this.asistente = asistente;
+    }
+
+    public int getNumAsiento() {
+        return numAsiento;
+    }
+
+    public void setNumAsiento(int numAsiento) {
+        this.numAsiento = numAsiento;
+    }
+
+    public char getSeccionAsiento() {
+        return seccionAsiento;
+    }
+
+    public void setSeccionAsiento(char seccionAsiento) {
+        this.seccionAsiento = seccionAsiento;
+    }
+
+    public Festival getFestival() {
+        return festival;
+    }
+
+    public void setFestival(Festival festival) {
+        this.festival = festival;
+    }
+    
     
 }
