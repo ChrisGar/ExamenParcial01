1f090eeb15302d8a648154fca02940889e449bae
Estudiante <Estudiante@lab0804>
2017-11-28 14:27:45 +0100

    Se creo la clase asistente

---
+++ src/Asistente.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Asistente extends Persona{
+    
+    private Boleto boleto;
+
+    public Asistente(Boleto boleto) {
+        this.boleto = boleto;
+    }
+
+    public Boleto getBoleto() {
+        return boleto;
+    }
+
+    public void setBoleto(Boleto boleto) {
+        this.boleto = boleto;
+    }
+    
+    
+}
